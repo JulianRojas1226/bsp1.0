79d0e3447abe2fd4b6e3e62377458baf851288c1
JulianRojas1226 <julisrojas26@gmail.com>
2025-06-25 02:13:19 -0500

    cambio arreglo bd  y archivo

--- config/db.js
+++ config/db.js
@@ -1,24 +1,5 @@
-// import-db.js
 import mysql from 'mysql2/promise';
-import fs from 'fs';
 
-async function importSQL() {
-  const connection = await mysql.createConnection(process.env.DATABASE_URL);
-  
-  // Leer archivo SQL
-  const sqlFile = fs.readFileSync('./database.sql', 'utf8');
-  
-  // Ejecutar cada statement
-  const statements = sqlFile.split(';').filter(stmt => stmt.trim());
-  
-  for (const statement of statements) {
-    if (statement.trim()) {
-      await connection.execute(statement);
-    }
-  }
-  
-  console.log('Base de datos importada exitosamente');
-  await connection.end();
-}
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
-importSQL().catch(console.error);
+export default db;
